04b1e63fb489d61f6253d2d55ae2c7a00f199a06
Abdul Rahman Al-Harbi <133687927+Abutamim3@users.noreply.github.com>
2023-05-21 03:02:26 +0300

    Add files via upload

---
+++ Execute an external program.py
@@ -0,0 +1,20 @@
+'''02-Execute an external program'''
+#Developed by Abdul Rahman Al-Harbi
+
+
+
+#import 
+import os
+
+
+
+os.startfile ("C:\Windows\System32\SnippingTool.exe")
+os.startfile ("C:\Windows\System32\MRT.exe")
+os.startfile ("C:\Windows\System32\calc.exe")
+os.startfile ("C:\Windows\System32\cleanmgr.exe")
+os.startfile ("C:\Windows\System32\cmd.exe")
+os.startfile ("C:\Windows\System32\dxdiag.exe")
+
+
+
+print("Developed by Abdul Rahman Al-Harbi")
